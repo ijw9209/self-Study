7b015739fae245740195a12a0e24e821c1739af6
ijw9209 <ijw9209@naver.com>
2020-03-16 18:24:05 +0900

    19-2 create 구현 : mode 변경가능

---
+++ react/react-review/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Subject from './Components/Subject';
+import TOC from './Components/TOC';
+import Content from './Components/Content';
+import Controll from './Components/Controll'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'read',
+      selected_content_id: 2,
+      subject: { title: "WEB", sub: "world wide web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is HyperText Markup Language.' },
+        { id: 2, title: 'CSS', desc: 'Css hello' },
+        { id: 3, title: 'JavaScript', desc: 'javaScript hello' }
+      ]
+    }
+  }
+  render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
+
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({
+              mode: 'welcome'
+            })
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC onChangePage={function (id) {
+          alert('hi');
+          this.setState({
+            mode: 'read',
+            selected_content_id: Number(id)
+          })
+        }.bind(this)}
+          data={this.state.contents}></TOC>
+        <Controll
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode
+            })
+          }.bind(this)}></Controll>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
+    )
+  }
+}
+
+export default App;
