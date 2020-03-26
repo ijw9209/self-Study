4413d0231cefb7f0549f2d3d409ff864da8003bb
ijw9209 <ijw9209@naver.com>
2020-03-27 00:06:16 +0900

    react-immer

--- react/react-practice/src/Components/TodoList.js
+++ react/react-practice/src/Components/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoList extends Component {
+
     render() {
         console.log("todolist render!!")
         const { items, clearList, handleDelete, handleEdit } = this.props;
