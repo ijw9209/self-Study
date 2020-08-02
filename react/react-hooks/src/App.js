93ca2c82c7cc0579c40e19f643536a5594336a16
ijw9209 <ijw9209@naver.com>
2020-08-02 20:54:25 +0900

    15. 배열에 항목 수정하기

--- react/react-hooks/src/App.js
+++ react/react-hooks/src/App.js
@@ -37,17 +37,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -78,6 +81,13 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user => user.id === id ?
+        { ...user, active: !user.active } : user))
+
+  }
+
   return (
     <>
       <CreateUser
@@ -86,7 +96,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
